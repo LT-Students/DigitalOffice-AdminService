b0c900279859b98c9ddf2a24456e515146652e44
Ekaterina Ryzhenkova <vadivazikryzh@gmail.com>
2021-11-19 05:30:21 +0300

    fix

--- src/AdminService/Program.cs
+++ src/AdminService/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace AdminService
 {
@@ -7,7 +10,42 @@ namespace AdminService
     {
         public static void Main(string[] args)
         {
-          CreateHostBuilder(args).Build().Run();
+          var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string seqServerUrl = Environment.GetEnvironmentVariable("seqServerUrl");
+            if (string.IsNullOrEmpty(seqServerUrl))
+            {
+                seqServerUrl = configuration["Serilog:WriteTo:1:Args:serverUrl"];
+            }
+
+            string seqApiKey = Environment.GetEnvironmentVariable("seqApiKey");
+            if (string.IsNullOrEmpty(seqApiKey))
+            {
+                seqApiKey = configuration["Serilog:WriteTo:1:Args:apiKey"];
+            }
+
+            Log.Logger = new LoggerConfiguration().ReadFrom
+                .Configuration(configuration)
+                .Enrich.WithProperty("Service", "CompanyService")
+                .WriteTo.Seq(
+                    serverUrl: seqServerUrl,
+                    apiKey: seqApiKey)
+                .CreateLogger();
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exc)
+            {
+                Log.Fatal(exc, "Can not properly start CompanyService.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- src/AdminService/Startup.cs
+++ src/AdminService/Startup.cs
@@ -1,51 +1,280 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using HealthChecks.UI.Client;
+using AdminService.Models.Dto.Configuration;
+using LT.DigitalOffice.Kernel.Configurations;
+using LT.DigitalOffice.Kernel.Extensions;
+using LT.DigitalOffice.Kernel.Middlewares.ApiInformation;
+using LT.DigitalOffice.Kernel.Middlewares.Token;
+using MassTransit;
+using MassTransit.RabbitMqTransport;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MassTransit;
+using Serilog;
+using System.Text.RegularExpressions;
+using LT.DigitalOffice.Kernel.Helpers;
+using LT.DigitalOffice.Kernel.Helpers.Interfaces;
+using AdminService.Data.Provider.MsSql.Ef;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using LT.DigitalOffice.CompanyService.Broker.Consumers;
+using LT.DigitalOffice.UserService.Models.Dto.Configurations;
 
 namespace AdminService
 {
-  public class Startup
+  public class Startup : BaseApiInfo
   {
+    public const string CorsPolicyName = "LtDoCorsPolicy";
 
     private readonly RabbitMqConfig _rabbitMqConfig;
-    //private readonly BaseServiceInfoConfig _serviceInfoConfig;
+    private readonly BaseServiceInfoConfig _serviceInfoConfig;
 
-
-
-
+    private IConfiguration Configuration { get; }
 
     public Startup(IConfiguration configuration)
     {
       Configuration = configuration;
+
+      _rabbitMqConfig = Configuration
+        .GetSection(BaseRabbitMqConfig.SectionName)
+        .Get<RabbitMqConfig>();
+
+      _serviceInfoConfig = Configuration
+        .GetSection(BaseServiceInfoConfig.SectionName)
+        .Get<BaseServiceInfoConfig>();
+
+      Version = "1.0.0.0";
+      Description = "AdminService is an API that intended to work with app installation.";
+      StartTime = DateTime.UtcNow;
+      ApiName = $"LT Digital Office - {_serviceInfoConfig.Name}";
     }
 
-    public IConfiguration Configuration { get; }
+   #region public methods
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddControllers();
+      services.AddCors(options =>
+      {
+        options.AddPolicy(
+          CorsPolicyName,
+          builder =>
+          {
+            builder
+              .AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+          });
+      });
+
+       if (int.TryParse(Environment.GetEnvironmentVariable("RedisCacheLiveInMinutes"), out int redisCacheLifeTime))
+       {
+         services.Configure<RedisConfig>(options =>
+         {
+           options.CacheLiveInMinutes = redisCacheLifeTime;
+         });
+       }
+       else
+       {
+         services.Configure<RedisConfig>(Configuration.GetSection(RedisConfig.SectionName));
+       }
+
+      services.Configure<TokenConfiguration>(Configuration.GetSection("CheckTokenMiddleware"));
+      services.Configure<BaseRabbitMqConfig>(Configuration.GetSection(BaseRabbitMqConfig.SectionName));
+      services.Configure<BaseServiceInfoConfig>(Configuration.GetSection(BaseServiceInfoConfig.SectionName));
+
+      services.AddHttpContextAccessor();
+      services
+        .AddControllers()
+        .AddJsonOptions(options =>
+        {
+          options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        })
+        .AddNewtonsoftJson();
+
+      string connStr = Environment.GetEnvironmentVariable("ConnectionString");
+      if (string.IsNullOrEmpty(connStr))
+      {
+        connStr = Configuration.GetConnectionString("SQLConnectionString");
+
+        Log.Information($"SQL connection string from appsettings.json was used. Value '{HidePassord(connStr)}'.");
+      }
+      else
+      {
+        Log.Information($"SQL connection string from environment was used. Value '{HidePassord(connStr)}'.");
+      }
+
+      services.AddDbContext<AdminServiceDbContext>(options =>
+      {
+        options.UseSqlServer(connStr);
+      });
+
+      services.AddHealthChecks()
+        .AddSqlServer(connStr)
+        .AddRabbitMqCheck();
+
+      string redisConnStr = Environment.GetEnvironmentVariable("RedisConnectionString");
+      if (string.IsNullOrEmpty(redisConnStr))
+      {
+        redisConnStr = Configuration.GetConnectionString("Redis");
+
+        Log.Information($"Redis connection string from appsettings.json was used. Value '{HidePassord(redisConnStr)}'");
+      }
+      else
+      {
+        Log.Information($"Redis connection string from environment was used. Value '{HidePassord(redisConnStr)}'");
+      }
+
+      services.AddSingleton<IConnectionMultiplexer>(
+        x => ConnectionMultiplexer.Connect(redisConnStr));
+
+      services.AddTransient<ICacheNotebook, CacheNotebook>();
+      services.AddTransient<IRedisHelper, RedisHelper>();
+
+      services.AddBusinessObjects();
+
+      ConfigureMassTransit(services);
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
     {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
+      UpdateDatabase(app);
+
+      app.UseForwardedHeaders();
+
+      app.UseExceptionsHandler(loggerFactory);
+
+      app.UseApiInformation();
 
       app.UseRouting();
 
-      app.UseAuthorization();
+      app.UseMiddleware<TokenMiddleware>();
+
+      app.UseCors(CorsPolicyName);
 
       app.UseEndpoints(endpoints =>
       {
-        endpoints.MapControllers();
+        endpoints.MapControllers().RequireCors(CorsPolicyName);
+
+        endpoints.MapHealthChecks($"/{_serviceInfoConfig.Id}/hc", new HealthCheckOptions
+        {
+          ResultStatusCodes = new Dictionary<HealthStatus, int>
+          {
+            { HealthStatus.Unhealthy, 200 },
+            { HealthStatus.Healthy, 200 },
+            { HealthStatus.Degraded, 200 },
+          },
+          Predicate = check => check.Name != "masstransit-bus",
+          ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
       });
     }
+
+    #endregion
+
+    #region private methods
+
+    private (string username, string password) GetRabbitMqCredentials()
+    {
+      static string GetString(string envVar, string formAppsettings, string generated, string fieldName)
+      {
+        string str = Environment.GetEnvironmentVariable(envVar);
+        if (string.IsNullOrEmpty(str))
+        {
+          str = formAppsettings ?? generated;
+
+          Log.Information(
+            formAppsettings == null
+              ? $"Default RabbitMq {fieldName} was used."
+              : $"RabbitMq {fieldName} from appsetings.json was used.");
+        }
+        else
+        {
+          Log.Information($"RabbitMq {fieldName} from environment was used.");
+        }
+
+        return str;
+      }
+
+      return (GetString("RabbitMqUsername", _rabbitMqConfig.Username, $"{_serviceInfoConfig.Name}_{_serviceInfoConfig.Id}", "Username"),
+        GetString("RabbitMqPassword", _rabbitMqConfig.Password, _serviceInfoConfig.Id, "Password"));
+    }
+
+     private void ConfigureMassTransit(IServiceCollection services)
+     {
+      (string username, string password) = GetRabbitMqCredentials();
+
+      services.AddMassTransit(x =>
+      {
+        x.AddConsumer<GetSmtpCredentialsConsumer>();
+
+        x.UsingRabbitMq((context, cfg) =>
+        {
+            cfg.Host(_rabbitMqConfig.Host, "/", host =>
+            {
+                host.Username(username);
+                host.Password(password);
+            });
+
+            ConfigureEndpoints(context, cfg);
+        });
+
+        x.AddRequestClients(_rabbitMqConfig);
+      });
+
+      services.AddMassTransitHostedService();
+     }
+
+     private void ConfigureEndpoints(
+      IBusRegistrationContext context,
+      IRabbitMqBusFactoryConfigurator cfg)
+     {
+      cfg.ReceiveEndpoint(_rabbitMqConfig.GetSmtpCredentialsEndpoint, ep =>
+      {
+        ep.ConfigureConsumer<GetSmtpCredentialsConsumer>(context);
+      });
+     }
+
+     private void UpdateDatabase(IApplicationBuilder app)
+     {
+      using var serviceScope = app.ApplicationServices
+        .GetRequiredService<IServiceScopeFactory>()
+        .CreateScope();
+
+      using var context = serviceScope.ServiceProvider.GetService<AdminServiceDbContext>();
+
+      context.Database.Migrate();
+     }
+
+     private string HidePassord(string line)
+     {
+      string password = "Password";
+
+      int index = line.IndexOf(password, 0, StringComparison.OrdinalIgnoreCase);
+
+      if (index != -1)
+      {
+        string[] words = Regex.Split(line, @"[=,; ]");
+
+        for (int i = 0; i < words.Length; i++)
+        {
+          if (string.Equals(password, words[i], StringComparison.OrdinalIgnoreCase))
+          {
+            line = line.Replace(words[i + 1], "****");
+            break;
+          }
+        }
+      }
+
+      return line;
+     }
+
+     #endregion
   }
 }
