0f4abf84482bebaee35d266fdf1358274d99abc8
Ekaterina Ryzhenkova <vadivazikryzh@gmail.com>
2021-11-20 23:55:09 +0300

    fix IDataProvider and add controller

--- src/AdminService.Data.Provider.MsSql.Ef/AdminServiceDbContext.cs
+++ src/AdminService.Data.Provider.MsSql.Ef/AdminServiceDbContext.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace LT.DigitalOffice.AdminService.Data.Provider.MsSql.Ef
 {
-    public class AdminServiceDbContext : DbContext, IDataProvider
-    {
+  public class AdminServiceDbContext : DbContext, IDataProvider
+  {
     public AdminServiceDbContext(DbContextOptions<AdminServiceDbContext> options)
-      : base(options)
+    : base(options)
     {
     }
+
+    // Fluent API is written here.
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.ApplyConfigurationsFromAssembly(Assembly.Load("LT.DigitalOffice.AdminService.Models.Db"));
     }
+
+    public void Save()
+    {
+      SaveChanges();
+    }
+
+    public object MakeEntityDetached(object obj)
+    {
+      Entry(obj).State = EntityState.Detached;
+
+      return Entry(obj).State;
+    }
+
+    public void EnsureDeleted()
+    {
+      Database.EnsureDeleted();
+    }
+
+    public bool IsInMemory()
+    {
+      return Database.IsInMemory();
+    }
+
+    public async Task SaveAsync()
+    {
+      await SaveChangesAsync();
+    }
+
+  }
 }
---
+++ src/AdminService/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LT.DigitalOffice.AdminService.Controllers
+{
+  [Route("[controller]")]
+  [ApiController]
+  public class AdminController : ControllerBase
+  {
+
+  }
+}
