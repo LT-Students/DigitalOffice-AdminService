2948575bcdc4bf669eec225405dbb3580f0172e1
asyagraf <67205781+asyagraf@users.noreply.github.com>
2021-11-24 12:09:53 +0300

    fix IServiceConfigurationRepository

--- src/AdminService.Data/Interfaces/IServiceConfigurationRepository.cs
+++ src/AdminService.Data/Interfaces/IServiceConfigurationRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LT.DigitalOffice.AdminService.Data.Interfaces
 {
-    public class IServiceConfigurationRepository
+    public interface IServiceConfigurationRepository
     {
     }
 }
