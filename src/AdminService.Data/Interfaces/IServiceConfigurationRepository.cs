df64d4b164df3f4f97e0b649cedcdd07e3858185
asyagraf <67205781+asyagraf@users.noreply.github.com>
2021-11-24 12:22:25 +0300

    Update IServiceConfigurationRepository.cs

--- src/AdminService.Data/Interfaces/IServiceConfigurationRepository.cs
+++ src/AdminService.Data/Interfaces/IServiceConfigurationRepository.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LT.DigitalOffice.AdminService.Data.Interfaces
+﻿namespace LT.DigitalOffice.AdminService.Data.Interfaces
 {
-    public interface IServiceConfigurationRepository
-    {
-    }
+  public interface IServiceConfigurationRepository
+  {
+  }
 }
