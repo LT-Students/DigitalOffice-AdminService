3736b8fe669a84c2e2bc6c30a317262200646c41
Mariya <mariyasin@list.ru>
2022-01-14 11:12:16 +0300

    fixes

--- src/AdminService.Data/Interfaces/IServiceEndpointRepository.cs
+++ src/AdminService.Data/Interfaces/IServiceEndpointRepository.cs
@@ -8,7 +8,7 @@ namespace LT.DigitalOffice.AdminService.Data.Interfaces
   [AutoInject]
   public interface IServiceEndpointRepository
   {
-    Task<bool> CreateAsync(List<DbServiceEndpoint> servicesEndpoints);
+    Task<bool> CreateAsync(List<DbServiceEndpoint> dbServicesEndpoints);
 
     Task<DbServiceConfiguration> GetAsync(string serviceName);
   }
